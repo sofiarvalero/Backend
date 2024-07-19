732db247600891bf5ce285cd1dde73a0c2aaa330
jesusSalazar14 <salazargodoyja@uvm.edu.ve>
2024-07-19 13:33:01 -0400

    Actualizado el archivo de migracion de la BD

--- bancouvm.sql
+++ bancouvm.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 22-06-2024 a las 21:09:59
+-- Tiempo de generación: 19-07-2024 a las 19:32:21
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -103,6 +103,7 @@ CREATE TABLE `usuarios` (
   `clave` varchar(255) NOT NULL,
   `cedula` int(9) NOT NULL,
   `telefono` varchar(11) NOT NULL,
+  `tipo` varchar(15) NOT NULL DEFAULT 'usuario',
   `id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
